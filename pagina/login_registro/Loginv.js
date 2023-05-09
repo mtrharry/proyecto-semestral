237a737741db86e3064f04c0d4a446b65feb9c6a
TheWarimin <93553763+TheWarimin@users.noreply.github.com>
2023-05-09 19:36:58 -0400

    Add files via upload
    formulario y api + arreglo de barra reactiva

---
+++ pagina/login_registro/Loginv.js
@@ -0,0 +1,18 @@
+
+function login(){
+  document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username === "harry" && password === "1234") {
+       
+        window.location="../index.html"
+    }  else {
+        // login failed
+        document.getElementById("error-message").innerHTML = "Usuario no registrado.";
+    }
+});
+  
+  
+}
+
