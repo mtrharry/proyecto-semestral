7f11a580481416e5ff9396bbb5e0bbb081836d4d
TheWarimin <ivostambuk7@gmail.com>
2023-06-23 16:19:40 -0400

    registro avance

--- newPage/views.py
+++ newPage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django import forms
 from .models import Usuario, tipoUsuario
-
 # Create your views here.
 
 def blog(request):
@@ -49,36 +49,71 @@ def crud(request):
     return render(request, "pages/user_list.html", context)
 
 
-def userAdd(request):
-    if request.method != "POST":
-        tipo = tipoUsuario.objects.all()
-        context = {"tipo": tipo}
-        return render(request, "pages/user_add.html", context)
-    else:
-        rut = request.POST["rut"]
-        nombre = request.POST["nombre"]
-        appPaterno = request.POST["appPaterno"]
-        appMaterno = request.POST["appMaterno"]
-        fecha = request.POST["fecha"]
-        tipo = request.POST["tipoUsuario"]
-        correo = request.POST["correo"]
-        telefono = request.POST["telefono"]
+class UserAddForm(forms.Form):
+    rut = forms.CharField(max_length=10)
+    nombre = forms.CharField(max_length=50)
+    appPaterno = forms.CharField(max_length=30)
+    appMaterno = forms.CharField(max_length=30)
+    fecha = forms.DateField()
+    correo = forms.EmailField()
+    telefono = forms.CharField(max_length=10)
 
-        objTipo = tipoUsuario.objects.get(idTipoUsuario=tipo)
-        objUsuario = Usuario.objects.create(
-            rut=rut,
-            nombre=nombre,
-            appPaterno=appPaterno,
-            appMaterno=appMaterno,
-            fechaNacimiento=fecha,
-            tipoUsuario=objTipo,
-            correo=correo,
-            telefono=telefono,
-            activo=1,
-        )
-        objUsuario.save()
-        context = {"mensaje": "OK Registrado Correctamente"}
-        return render(request, "pages/user_add.html", context)
+def userAdd(request):
+    if request.method == "POST":
+        form = UserAddForm(request.POST)
+        if form.is_valid():
+            rut = form.cleaned_data["rut"]
+            nombre = form.cleaned_data["nombre"]
+            appPaterno = form.cleaned_data["appPaterno"]
+            appMaterno = form.cleaned_data["appMaterno"]
+            fecha = form.cleaned_data["fecha"]
+            correo = form.cleaned_data["correo"]
+            telefono = form.cleaned_data["telefono"]
+            password = request.POST["password"]
+            direccion = request.POST["direccion"]
+
+            # Check if rut, nombre, and correo already exist in the database
+            if Usuario.objects.filter(rut=rut).exists():
+                form.add_error("rut", "El rut ya está registrado.")
+            if Usuario.objects.filter(nombre=nombre).exists():
+                form.add_error("nombre", "El nombre ya está registrado.")
+            if Usuario.objects.filter(correo=correo).exists():
+                form.add_error("correo", "El correo ya está registrado.")
+
+            if not form.errors:
+                tipo_cliente, created = tipoUsuario.objects.get_or_create(tipoUsuario="cliente")
+
+                objUsuario = Usuario(
+                    rut=rut,
+                    nombre=nombre,
+                    appPaterno=appPaterno,
+                    appMaterno=appMaterno,
+                    fechaNacimiento=fecha,
+                    tipoUsuario=tipo_cliente,
+                    correo=correo,
+                    telefono=telefono,
+                    activo=1,
+                    password=password,
+                    direccion = direccion,
+                )
+                objUsuario.save()
+
+                context = {"mensaje": "Registrado Correctamente"}
+                return render(request, "pages/registro.html", context)
+        else:
+            print("Form errors:", form.errors)
+    else:
+        form = UserAddForm()
+
+    tipo = tipoUsuario.objects.all()
+    context = {"form": form, "tipo": tipo}
+    return render(request, "pages/registro.html", context)
+
+
+
+
+
+
 
 
 def userDel(request, pk):
