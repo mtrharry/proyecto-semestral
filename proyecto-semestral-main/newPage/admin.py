2ad9272b074e1a7d2e10671c5dc0e0d7d61cb59a
TheWarimin <93553763+TheWarimin@users.noreply.github.com>
2023-06-25 03:54:16 -0400

    Delete proyecto-semestral-main directory

--- proyecto-semestral-main/newPage/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from .models import tipoUsuario, Usuario
-
-admin.site.register(tipoUsuario)
-admin.site.register(Usuario) 
--- proyecto-semestral-main/newPage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewpageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newPage'
